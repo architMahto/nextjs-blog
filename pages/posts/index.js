3350c6b8843fe6f65a3133b6bc0b08de925a59e8
architMahto <architmahto@gmail.com>
2022-04-25 21:50:12 -0400

    feat: rendered all posts page with dummy data

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,43 @@
+import PostsGrid from '../../components/posts/postsGrid'
+
+import classes from './allPosts.module.scss'
+
+const DUMMY_POSTS = [
+  {
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React frameword for production - it makes building full stack React apps and sites a breeze and ships with build-in SSR.',
+    date: '2022-02-10',
+    slug: 'getting-started-with-nextjs'
+  },
+  {
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React frameword for production - it makes building full stack React apps and sites a breeze and ships with build-in SSR.',
+    date: '2022-02-10',
+    slug: 'getting-started-with-nextjs2'
+  },
+  {
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React frameword for production - it makes building full stack React apps and sites a breeze and ships with build-in SSR.',
+    date: '2022-02-10',
+    slug: 'getting-started-with-nextjs3'
+  },
+  {
+    title: 'Getting Started with NextJS',
+    image: 'getting-started-nextjs.png',
+    excerpt: 'NextJS is the React frameword for production - it makes building full stack React apps and sites a breeze and ships with build-in SSR.',
+    date: '2022-02-10',
+    slug: 'getting-started-with-nextjs4'
+  },
+]
+
 export default function AllPosts() {
-  return (<p>Posts</p>)
+  return (
+    <section className={classes.posts}>
+      <h1>All Posts</h1>
+      <PostsGrid posts={DUMMY_POSTS} />
+    </section>
+  )
 }
