d22cb3569f62cd9261aa95bd86ee71dbbbef32b7
architMahto <architmahto@gmail.com>
2022-04-25 22:12:13 -0400

    feat: rendered post details page with dummy data and no markdown

--- pages/posts/[postSlug].js
+++ pages/posts/[postSlug].js
@@ -1,3 +1,22 @@
+import PostHeader from '../../components/posts/postHeader'
+
+import classes from './postDetails.module.scss'
+
+const DUMMY_POST = {
+  title: 'Getting Started with NextJS',
+  image: 'getting-started-nextjs.png',
+  date: '2022-02-10',
+  slug: 'getting-started-with-nextjs',
+  content: '# This is a first post'
+}
+
 export default function PostDetails() {
-  return (<p>Post Details</p>)
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      {DUMMY_POST.content}
+    </article>
+  )
 }
