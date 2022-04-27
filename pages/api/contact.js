438b0823164375b275309847d9c54cf80e797c53
architMahto <architmahto@gmail.com>
2022-04-27 15:40:16 -0400

    feat: added logic to store messages in mongodb

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb'
+
+export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { email, name, message: messageContent } = req.body
 
@@ -14,10 +16,33 @@ export default function handler(req, res) {
       return
     }
 
-    const messageBody = { email, name, messageContent }
-    res.status(201).json({
-      message: 'Successfully stored message!',
-      messageBody
-    })
+    let client
+    const MONGODB_USERNAME = 'mongoadmin'
+    const MONGODB_PASSWORD = 'testadmin1'
+    const MONGODB_DATABASE = 'nextjs-blog'
+    const MONGODB_URL = `mongodb+srv://${MONGODB_USERNAME}:${MONGODB_PASSWORD}@cluster0.03r3c.mongodb.net/${MONGODB_DATABASE}?retryWrites=true&w=majority`
+
+    try {
+      client = await MongoClient.connect(MONGODB_URL)
+    } catch (err) {
+      res.status(500).json({ status: 'Failed', message: 'Could not connect to database'})
+      return
+    }
+
+    const db = client.db()
+    const data = { email, name, messageContent }
+
+    try {
+      const result = await db.collection('messages').insertOne(data)
+      data.id = result._id
+    } catch (err) {
+      client.close()
+      res.status(500).json({ status: 'Failed', message: 'Could not store message'})
+      return
+    }
+
+    client.close()
+
+    res.status(201).json({ message: 'Successfully stored message!', data })
   }
 }
